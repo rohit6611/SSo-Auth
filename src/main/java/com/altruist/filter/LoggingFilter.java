b06b880a68884f84572aba93bd49d5bbe6013d28
rohitchopra6611 <rohit.chopra@altruistindia.com>
2024-04-05 17:24:15 +0530

    commited by rohit

--- src/main/java/com/altruist/filter/LoggingFilter.java
+++ src/main/java/com/altruist/filter/LoggingFilter.java
@@ -19,43 +19,43 @@ import lombok.extern.slf4j.Slf4j;
 
 
 
-//@Component
-//@Slf4j
-//public class LoggingFilter extends OncePerRequestFilter {
-//
-//	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
-//
-//
-//	private String getStringValue(byte[] contentAsByteArray, String characterEncoding) {
-//		try {
-//			return new String(contentAsByteArray, 0, contentAsByteArray.length, characterEncoding);
-//		} catch (UnsupportedEncodingException e) {
-//			e.printStackTrace();
-//		}
-//		return "";
-//	}
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//            throws ServletException, IOException {
-//        // TODO Auto-generated method stub
-//        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
-//		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
-//
-//		long startTime = System.currentTimeMillis();
-//		filterChain.doFilter(requestWrapper, responseWrapper);
-//		long timeTaken = System.currentTimeMillis() - startTime;
-//
-//		String requestBody = getStringValue(requestWrapper.getContentAsByteArray(),
-//				request.getCharacterEncoding());
-//		String responseBody = getStringValue(responseWrapper.getContentAsByteArray(),
-//				response.getCharacterEncoding());
-//
-//		log.info(
-//				"FINISHED PROCESSING : METHOD={}; REQUESTURI={}; REQUEST PAYLOAD={}; RESPONSE CODE={}; RESPONSE={}; TIME TAKEN={}",
-//				request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody,
-//				timeTaken+"ms");
-//		responseWrapper.copyBodyToResponse();
-//    }
-//
-//}
+@Component
+@Slf4j
+public class LoggingFilter extends OncePerRequestFilter {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
+
+
+	private String getStringValue(byte[] contentAsByteArray, String characterEncoding) {
+		try {
+			return new String(contentAsByteArray, 0, contentAsByteArray.length, characterEncoding);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+		ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+		long startTime = System.currentTimeMillis();
+		filterChain.doFilter(requestWrapper, responseWrapper);
+		long timeTaken = System.currentTimeMillis() - startTime;
+
+		String requestBody = getStringValue(requestWrapper.getContentAsByteArray(),
+				request.getCharacterEncoding());
+		String responseBody = getStringValue(responseWrapper.getContentAsByteArray(),
+				response.getCharacterEncoding());
+
+		log.info(
+				"FINISHED PROCESSING : METHOD={}; REQUESTURI={}; REQUEST PAYLOAD={}; RESPONSE CODE={}; RESPONSE={}; TIME TAKEN={}",
+				request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody,
+				timeTaken+"ms");
+		responseWrapper.copyBodyToResponse();
+    }
+
+}
